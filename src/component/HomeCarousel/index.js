a7c39225f565944e84da27b1a896beea84f4aa8c
ariqyandri <ariq140199@gmail.com>
2021-01-21 22:50:25 +0100

    FInish carousel

--- src/component/HomeCarousel/index.js
+++ src/component/HomeCarousel/index.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { Carousel } from "react-bootstrap";
+import { Button, Carousel } from "react-bootstrap";
+import "./HomeCarousel.css";
 
 export default function HomeCarousel({ categories }) {
   console.log(categories);
   return (
     <div>
-      <Carousel>
+      <Carousel wrap="true" interval={5000}>
         {categories.map(({ name, imageUrl, description }) => {
           return (
             <Carousel.Item>
-              <img className="d-block w-100" src={imageUrl} alt={name} />
+              <img className="d-block w-100 height" src={imageUrl} alt={name} />
               <Carousel.Caption>
                 <h1>{name}</h1>
-                <h3>{description}</h3>
+                <b />
+                <h3>Visit Page</h3>
               </Carousel.Caption>
             </Carousel.Item>
           );
