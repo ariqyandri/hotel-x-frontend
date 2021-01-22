fb76a3688dce6334b5a185e2016b156cd81a9ef6
ariqyandri <ariq140199@gmail.com>
2021-01-22 12:25:48 +0100

    Finish footer and contact

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -9,6 +9,7 @@ export default function Footer() {
   const loading = useSelector(selectAppLoading);
   const about = useSelector(selectAbout);
   const contacts = useSelector(selectContacts);
+  const { address, phone, email } = about;
   if (loading) {
     return <div></div>;
   }
@@ -17,9 +18,25 @@ export default function Footer() {
       <div className="page-margin footer-content">
         <section>
           <h1>Hotel X Amsterdam</h1>
+          {[{ address }, { phone }, { email }].map((info, i) => {
+            return (
+              <div className="footer-info" key={i}>
+                <p className="info">{info[`${Object.keys(info)[0]}`]}</p>
+              </div>
+            );
+          })}
         </section>
         <section>
-          <p></p>
+          {contacts.map(({ platform, info, linkUrl }, i) => {
+            if (platform === "Telephone" || platform === "Email") {
+              return;
+            }
+            return (
+              <p key={i}>
+                <span>{platform}</span> <a href={linkUrl}>{info}</a>
+              </p>
+            );
+          })}
         </section>
       </div>
     </div>
